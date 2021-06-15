1e9f580c9e62d2c93f2de261fdaa61f023dc25e4
Xudong Zou <zou.xudong@foxmail.com>
2021-06-15 10:35:10 +0800

    Update extractExon_and_Intron_from_gtf.py
    add one function to produce gene range

--- scripts/extractExon_and_Intron_from_gtf.py
+++ scripts/extractExon_and_Intron_from_gtf.py
@@ -58,7 +58,7 @@ def extract_exon(gtf_file,skip_rows):
 				continue
 	return transcript_exon
 
-
+# get the intron(s) from a exon list in a transcript
 def extract_intron(exon_list):
 	intron_list = []
 	for i in range(len(exon_list)-1):
@@ -81,19 +81,19 @@ def extract_gene_range(gtf_file,skip_rows):
 			chrn = w[0]
 			strand = w[6]
 			entry_type = w[2]
-			gene_start = int(w[3])
-			gene_end = int(w[4])
+			gene_start = w[3]
+			gene_end = w[4]
 			# build match patterns
 			geneid_patt_1 = re.search(r'gene_id\s"(\w+)";',w[8])
 			geneid_patt_2 = re.search(r'gene_id\s"(\w+\.\d+)";',w[8])# with version number
 			genename_patt = re.search(r'gene_name\s"(.*?)";',w[8])
 			genetype_patt = re.search(r'gene_type\s"(.*?)";',w[8])
 			if geneid_patt_1:
-				gene_id = geneid_patt_1.group(1)
+				geneid = geneid_patt_1.group(1)
 			elif geneid_patt_2:
-				gene_id = geneid_patt_2.group(1)
+				geneid = geneid_patt_2.group(1)
 			else:
-				gene_id = "NA"
+				geneid = "NA"
 			if genename_patt:
 				genename = genename_patt.group(1)
 			else:
@@ -103,21 +103,27 @@ def extract_gene_range(gtf_file,skip_rows):
 			else:
 				genetype = "NA"
 			if w[2]=="gene" and chrn in keep_chr:
-				gene_range[gene_id] = (chrn,str(gene_start-1),str(gene_end),strand,genetype)
+				gene_range[geneid] = (chrn,gene_start,gene_end,strand,genetype) # 1-based coordinates and with both ends included
 			else:
 				continue
 	return gene_range
+
+
 # Main -----------------------------------
 
+# define args 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--gtf',help="specify a gtf file")
 parser.add_argument('--out_exon',help="specify a filename for exon output")
 parser.add_argument('--out_intron',help="specify a filename for intron output")
+parser.add_argument('--geneRange', type=bool, default=True,action="store_true",help="if True, output gene range into a bed file: geneRange.bed")
 
-
+# interpret args
 args = parser.parse_args()
 
-exon_dict = extract_exon(args.gtf,5) # extract exons for each transcript
+# extract exon for each transcript
+exon_dict = extract_exon(args.gtf,5) # remove 5 header rows
+
 # print out exons in bed format
 fho = open(args.out_exon,'w')
 for t in exon_dict:
@@ -126,7 +132,7 @@ for t in exon_dict:
 		print >>fho, "%s\t%d\t%d\t%s\t%s\t%s\t%s\t%s" % (chrname,exon[0]-1,exon[1],gid+":"+tid,exon[1]-exon[0]+1,strand,gname,gtype)
 fho.close()
 
-
+# extract and output introns dependent on exon structures
 fho = open(args.out_intron,'w')
 for t in exon_dict:
 	chrname,tid,gid,strand,gname,gtype = t.split(":")
@@ -138,6 +144,20 @@ for t in exon_dict:
 
 			
 fho.close()
+
+# gene range
+if args.geneRange:
+	gene_range = extract_gene_range(args.gtf,5) # geneid:(chrn,gene_start,gene_end,strand,genetype)
+	fho = open("geneRange.bed",'w')
+	for gene in gene_range:
+		chrN = gene_range[gene][0]
+		startP = int(gene_range[gene][1])
+		endP = int(gene_range[gene][2])
+		strand = gene_range[gene][3]
+		genetype = gene_range[gene][4]
+		print >> fho, "%s\t%d\t%d\t%s\t%d\t%s" % (chrN,startP-1,endP,gene,endP-startP+1,strand)
+	fho.close()
+
 print "Done!"
 print "Generate exon bed in file:%s" % (args.out_exon)
 print "Generate intron bed in file:%s" % (args.out_intron)
